5af4a08767a8cdc38309054baf46221b81fcbd34
Nicolas Josino <nicolasjosino@gmail.com>
2026-03-03 18:48:47 -0300

    Ajustes

--- sales/src/main/java/com/pge/ecommerce/sales/infrastructure/client/ProductClient.java
+++ sales/src/main/java/com/pge/ecommerce/sales/infrastructure/client/ProductClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "products") // nome registrado no Eureka
+@FeignClient(name = "products")
 public interface ProductClient {
 
     @GetMapping("/api/products/{id}")
